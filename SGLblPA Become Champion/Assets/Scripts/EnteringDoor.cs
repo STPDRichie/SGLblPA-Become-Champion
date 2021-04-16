9333e7bc97f65e2fb46643c785fd527cb1d56b37
Коляска _ <nikolaj.karpov.2002@mail.ru>
2021-04-17 00:59:38 +0500

    Rich House Dungeon. "Press E" Prompt
    Закончен второй этаж данжа, добавлены телепорты
    
    Добавлен скрипт Prompt

--- SGLblPA Become Champion/Assets/Scripts/EnteringDoor.cs
+++ SGLblPA Become Champion/Assets/Scripts/EnteringDoor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnteringDoor : MonoBehaviour
 {
     private bool playerDetected;
+    
     public Transform doorPos;
     public float width;
     public float height;
@@ -25,8 +26,8 @@ public class EnteringDoor : MonoBehaviour
     {
         playerDetected = Physics2D.OverlapBox(doorPos.position, new Vector2(width, height), 0, whatIsPlayer);
 
-        if (playerDetected == true) 
-            if (Input.GetKeyDown(KeyCode.E)) 
+        if (playerDetected == true)
+            if (Input.GetKeyDown(KeyCode.E))
                 sceneSwitch.SwitchScene(sceneName);
     }
 
